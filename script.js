aa8c704dfec6d9abfb42e92d09d8a2fd12e094ab
BradMJ <90422753+BradMJ@users.noreply.github.com>
2023-10-29 09:03:39 -0700

    Add more selectors in html and remake Gameboard module in script

--- script.js
+++ script.js
@@ -12,112 +12,85 @@
 //   Or if all fields filled in and no three in a row,
 //     If gameOver(), no other fields can be filled in
 
-
-// ADDED THESE GLOBAL VARIABLES TO GAMEBOARD MODULE
-// const playDiv = document.querySelector('#playDiv');
-// const playBtn = document.querySelector('.playBtn');
-// const selectX = document.querySelector('.selectX');
-// const selectO = document.querySelector('.selectO');
-
-// // Play button
-//          ADDED TO GAMEBOARD MODULE
-// playBtn.addEventListener('click', () => {
-//     playBtn.style.display='none';
-//     selectX.style.display='block';
-//     selectO.style.display='block';
-//     selectX.addEventListener('click', () => {
-//         // assign choice to player here
-//         // assign other choice to computer here
-//         playDiv.style.display='none';
-//     });
-//     selectO.addEventListener('click', () => {
-//         //assign choice to player here
-//         // assign other choice to computer here
-//         playDiv.style.display='none';
-//     });
-// });
+'use strict';
 
 // IIFE module to create a single gameboard
-const Gameboard = (function(){
+const Gameboard = (() => {
+    // Element Selectors
     const playDiv = document.querySelector('#playDiv');
     const playBtn = document.querySelector('.playBtn');
     const selectX = document.querySelector('.selectX');
     const selectO = document.querySelector('.selectO');
-    const fields = document.querySelectorAll('.field');
+    const statusText = document.querySelector('#statusText');
+    // Global (private) variables
     const O = 'O';
     const X = 'X';
-    // const start = prompt('Choose X or O for Tic-Tac-Toe', 'X');
-    const startGame = () => {
-        let playerChoice = prompt('Choose X or O', 'X');
-        let computerChoice;
-        let playerMarker;
-        let computerMarker;
-        if (playerChoice == 'X') {
-            let playerMarker = X;
-            let computerMarker = O;
-        } else if (playerChoice == 'O') {
-            let playerMarker = O;
-            let computerMarker = X;
-        } else {
-            startGame();
-        };
-        return { playerMarker, computerMarker };
+    // let playerMarker;
+    // let computerMarker;
+
+    // Win conditions arrays
+    // const winConditions = [
+    //     [0, 1, 2],
+    //     [3, 4, 5],
+    //     [6, 7, 8],
+    //     [0, 3, 6],
+    //     [1, 4, 7],
+    //     [2, 5, 8],
+    //     [0, 4, 8],
+    //     [2, 4, 6]
+    // ];
+
+    // Render/load gameboard array
+    let board = ["", "", "", "", "", "", "", "", ""];
+
+    const setField = (index, sign) => {
+        if (index > board.length) return;
+        board[index] = sign;
     };
 
-    // Render/load gameboard
-    let gameboard = [];
-    function render() {
-        for (let i = 0; i < fields.length; i++) {
-            gameboard += fields[i];
+    const getField = (index) => {
+        if (index > board.length) return;
+        return board[index];
+    };
+
+    const reset = () => {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = "";
         };
     };
-    // Play button ADDED TO GAMEBOARD MODULE FROM GLOBAL SCOPE
-    playBtn.addEventListener('click', () => {
-        playBtn.style.display='none';
-        selectX.style.display='block';
-        selectO.style.display='block';
-        selectX.addEventListener('click', () => {
-            // assign choice to player here
-            // assign other choice to computer here
-            playDiv.style.display='none';
-            render();
-        });
-        selectO.addEventListener('click', () => {
-            //assign choice to player here
-            // assign other choice to computer here
-            playDiv.style.display='none';
-            render();
-        });
-    });
-    // Returns clicked field
-    //      Now assign choice of player or computer
-    fields.forEach(field => {
-        field.addEventListener('click', (e) => {
-            let target = e.target;
-            let parent = target.parentNode;
-            let index = [].indexOf.call(parent.children, target);
-            console.log(index);
-        });
-    });
-    return { startGame };
+    return { setField, getField, reset };
 })();
 
-// Object constructor Player object
-// function Player(name, marker) {
-//     this.name = name,
-//     this.marker = marker
-// }
+const displayController = (() => {
+    const fieldElements = document.querySelectorAll('.field');
+    const messageElement = document.querySelector('#message');
+    const restartBtn = document.querySelector('.restartBtn');
+
+    fieldElements.forEach((field) => {
+        field.addEventListener('click', (e) => {
+            if (GameController.getIsOver() || e.target.textContent !== "") return;
+            
+        })
+    })
+})();
 
 // Factory function Player object
-const Player = function(name, marker) {
-    let newPlayer = {};
-    newPlayer.name = name;
-    newPlayer.marker = marker;
-    return newPlayer;
-}
-const player1 = Player('Rick', "X")
-const player2 = Player('Joe', "O");
-console.log(player1.name);
-console.log(player1.marker);
-console.log(player2.name);
-console.log(player2.marker);
+const Player = (marker) => {
+    this.marker = marker;
+
+    const getMarker = () => {
+        return marker;
+    };
+    
+    return { getMarker };
+    // let newPlayer = {};
+    // newPlayer.name = name;
+    // newPlayer.marker = marker;
+    // return newPlayer;
+};
+// const player1 = Player('Rick', "X")
+// const player2 = Player('Joe', "O");
+// console.log(player1.name);
+// console.log(player1.marker);
+// console.log(player2.name);
+// console.log(player2.marker);
