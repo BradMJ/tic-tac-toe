311c556fd8dea37e816cc9f3cf3aa3a12502b3e1
BradMJ <90422753+BradMJ@users.noreply.github.com>
2023-11-02 10:28:17 -0700

    Remove play and marker choice buttons and add identifiers to all fields in html, and remove comments and reorganize working script

--- script.js
+++ script.js
@@ -1,46 +1,19 @@
-// Written pseudo-code below
-
-// Need a gameboard
-// Need gameboard reset button to clear all fields
-// Need computer opponent
-// Start button
-//   Button lets player pick X or O, auto assigns other
-//     On pick, Math.random who goes first, X or O
-// Computer pick needs random open field position
-// After every pick, function ran to check if three in a row
-//   If three in a row, winner declared,
-//   Or if all fields filled in and no three in a row,
-//     If gameOver(), no other fields can be filled in
-
 'use strict';
 
-// IIFE module to create a single gameboard
+// Factory function Player object
+const Player = (marker) => {
+    this.marker = marker;
+
+    const getMarker = () => {
+        return marker;
+    };
+    
+    return { getMarker };
+};
+
+// Gameboard IIFE module to create a single gameboard
 const Gameboard = (() => {
-    // Element Selectors
-    const playDiv = document.querySelector('#playDiv');
-    const playBtn = document.querySelector('.playBtn');
-    const selectX = document.querySelector('.selectX');
-    const selectO = document.querySelector('.selectO');
-    const statusText = document.querySelector('#statusText');
-    // Global (private) variables
-    const O = 'O';
-    const X = 'X';
-    // let playerMarker;
-    // let computerMarker;
-
-    // Win conditions arrays
-    // const winConditions = [
-    //     [0, 1, 2],
-    //     [3, 4, 5],
-    //     [6, 7, 8],
-    //     [0, 3, 6],
-    //     [1, 4, 7],
-    //     [2, 5, 8],
-    //     [0, 4, 8],
-    //     [2, 4, 6]
-    // ];
-
-    // Render/load gameboard array
+    // Render gameboard array
     let board = ["", "", "", "", "", "", "", "", ""];
 
     const setField = (index, sign) => {
@@ -61,6 +34,8 @@ const Gameboard = (() => {
     return { setField, getField, reset };
 })();
 
+// DisplayController IIFE module adds click events
+// and controls the flow of the game
 const DisplayController = (() => {
     const fieldElements = document.querySelectorAll('.field');
     const messageElement = document.querySelector('#message');
@@ -102,13 +77,66 @@ const DisplayController = (() => {
     return { setResultMessage, setMessageElement };
 })();
 
-// Factory function Player object
-const Player = (marker) => {
-    this.marker = marker;
+// GameController IIFE to show whos turn
+// and show win/tie and reset
+const GameController = (() => {
+    const playerX = Player('X');
+    const playerO = Player('O');
+    let round = 1;
+    let gameOver = false;
 
-    const getMarker = () => {
-        return marker;
+    const playRound = (fieldIndex) => {
+        Gameboard.setField(fieldIndex, getCurrentPlayerSign());
+        if (checkWinner(fieldIndex)) {
+            DisplayController.setResultMessage(getCurrentPlayerSign());
+            gameOver = true;
+            return;
+        };
+        if (round === 9) {
+            DisplayController.setResultMessage('Draw');
+            gameOver = true;
+            return;
+        };
+        round++;
+        DisplayController.setMessageElement(
+            `Player ${getCurrentPlayerSign()}'s turn`
+        );
     };
-    
-    return { getMarker };
-};
+
+    const getCurrentPlayerSign = () => {
+        return round % 2 === 1 ? playerX.getMarker() : playerO.getMarker();
+    };
+
+    const checkWinner = (fieldIndex) => {
+        // Win conditions arrays
+        const winConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        return winConditions
+            .filter((combination) => combination.includes(fieldIndex))
+            .some((possibleCombination) => 
+                possibleCombination.every(
+                    (index) => Gameboard.getField(index) === getCurrentPlayerSign()
+                )
+            );
+    };
+
+    const getIsOver = () => {
+        return gameOver;
+    };
+
+    const reset = () => {
+        round = 1;
+        gameOver = false;
+    };
+
+    return { playRound, getIsOver, reset };
+})();
